ce924e756fe7a4759186175183c4fa9558848b79
Hudson S Borges <hudsonsilbor@gmail.com>
2023-01-18 21:48:59 -0400

    feat: adiciona cache à api de queimadas

--- server/api/controllers/queimadas/router.ts
+++ server/api/controllers/queimadas/router.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import controller from './controller';
 
+import { middleware as cacheMiddleware } from 'apicache';
+
 export default Router()
-  .get('/municipio/:municipio', controller.municipio)
-  .get('/estado/:estado', controller.estado);
+  .get('/municipio/:municipio', cacheMiddleware('1 week'), controller.municipio)
+  .get('/estado/:estado', cacheMiddleware('1 week'), controller.estado);
