a004e525b84999137a8b78a7346b6a1ba1ad7f8d
Hudson Silva Borges <hudsonsilbor@gmail.com>
2023-05-11 18:50:57 -0400

    refactor: retorna todos estados/municipios e se estes possuem queimadas

--- server/api/services/mapas.service.ts
+++ server/api/services/mapas.service.ts
@@ -16,7 +16,7 @@ export async function entidadesComDados(type: 'mapas_municipios' | 'mapas_estado
   const { rowCount, rows } = await knex.raw(
     `
     WITH queimadas AS (SELECT ST_Union(ST_Simplify(mq.wkb_geometry, 0.1, TRUE)) AS wkb_geometry FROM mapas_queimadas mq)
-    SELECT DISTINCT ma.id, ma.nome FROM ${type} ma JOIN queimadas mq ON ST_Intersects(ST_Transform(ma.wkb_geometry, 4326), mq.wkb_geometry)
+    SELECT DISTINCT ma.id, ma.nome, (mq.wkb_geometry is not null) as queimadas FROM ${type} ma LEFT JOIN queimadas mq ON ST_Intersects(ST_Transform(ma.wkb_geometry, 4326), mq.wkb_geometry)
     `
   );
 
