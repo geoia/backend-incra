520fbfb9fe2c40288528d9e47785d7673c436cf8
Hudson S. Borges <hudsonsilbor@gmail.com>
2023-10-26 18:30:55 -0400

    feat: se dados ja foram obtidos, lança somente um aviso

--- scripts/maps-download.ts
+++ scripts/maps-download.ts
@@ -1,16 +1,21 @@
-import { program } from 'commander';
-import { createWriteStream } from 'node:fs';
+import { Option, program } from 'commander';
+import { createWriteStream, existsSync } from 'node:fs';
 import https from 'node:https';
 import { withFile } from 'tmp-promise';
 import decompress from 'decompress';
-import { join, resolve } from 'node:path';
+import { basename, join, resolve } from 'node:path';
 import consola from 'consola';
 import crypto from 'crypto';
 
 /***
  * Função auxiliar que faz o download e extrai arquivos.
  */
-async function download(url: string, dest: string): Promise<void> {
+async function download(url: string, dest: string, override = false): Promise<void> {
+  if (!override && existsSync(dest)) {
+    consola.info('Files already downloaded (%s), skipping...', basename(dest));
+    return;
+  }
+
   consola.info('Downloading %s', url);
   consola.debug('Preparando arquivos temporários ...');
   return withFile(async ({ path }) => {
@@ -37,17 +42,20 @@ async function download(url: string, dest: string): Promise<void> {
 
 if (require.main === module) {
   program
-    .action(async () => {
+    .addOption(new Option('--override', 'Override existing files'))
+    .action(async (opts: { override: boolean }) => {
       consola.info('Baixando mapas ...');
       const mapas = resolve(__dirname, '../shapefiles/mapas');
       await Promise.all([
         download(
           'https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2021/Brasil/BR/BR_Municipios_2021.zip',
-          join(mapas, 'BR_Municipios_2021')
+          join(mapas, 'BR_Municipios_2021'),
+          opts.override
         ),
         download(
           'https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2021/Brasil/BR/BR_UF_2021.zip',
-          join(mapas, 'BR_UF_2021')
+          join(mapas, 'BR_UF_2021'),
+          opts.override
         ),
       ]);
 
