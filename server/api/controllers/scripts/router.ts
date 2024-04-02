4080b8739282d2d5591f91dbfda6ac8a3046846a
rafael.bsilva <rafael.bsilva@cardway.com.br>
2024-04-02 16:50:41 -0400

    feat: adiciona api para executar scripts de queimadas

--- server/api/controllers/scripts/router.ts
+++ server/api/controllers/scripts/router.ts
@@ -5,4 +5,8 @@ import { middleware as cacheMiddleware } from 'apicache';
 
 export default Router()
   .post('/download-mapas', controller.downloadMapasController)
-  .post('/populate-municipios', controller.populateMunicipiosController);
+  .post('/populate-municipios', controller.populateMunicipiosController)
+  .post('/queimadas', controller.queimadasExecController)
+  .get('/queimadas/prefixes', controller.queimadasGetPrefixesController)
+  .delete('/queimadas/prefixes', controller.queimadasDeleteController)
+  .post('/queimadas/cron', controller.queimadasCronExecController);
