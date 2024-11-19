0b23f39d86b1d5db8df37e3097878db2cfe47b8f
Rafael de Azevedo <rafael.deazevedo01@gmail.com>
2024-11-19 08:30:32 -0400

    fix: ajusta rotas dos endpoints de assentamentos

--- server/api/controllers/assentamentos/router.ts
+++ server/api/controllers/assentamentos/router.ts
@@ -4,5 +4,5 @@ import controller from './controller';
 import { middleware as cacheMiddleware } from 'apicache';
 
 export default Router()
-  .get('/assentamentos', cacheMiddleware('1 week'), controller.find)
-  .get('/assentamentos/:id', cacheMiddleware('1 week'), controller.get);
+  .get('/', cacheMiddleware('1 week'), controller.find)
+  .get('/:id', cacheMiddleware('1 week'), controller.get);
